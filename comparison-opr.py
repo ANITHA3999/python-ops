293f908e669a45ef4310982d7cabf305a2db71a0
ANITHA3999 <anithareddy90900@gmail.com>
2025-07-02 18:37:14 +0530

    add examples of commparison operator

---
+++ comparison-opr.py
@@ -0,0 +1,9 @@
+a = 14
+b = 4
+
+print("greater than:", a > b)
+print("less than:", a < b)
+print("greater than or equal to:", a >= b)
+print("less than or equal to:", a <= b)
+print("equal to:", a == b)
+print("not equal to:", a != b)
