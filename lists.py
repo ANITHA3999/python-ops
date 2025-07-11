d71341d8ba23bfb4cf2ee350d0560521e0b1e2a7
ANITHA3999 <anithareddy90900@gmail.com>
2025-07-11 18:24:47 +0530

    create a new list

---
+++ lists.py
@@ -0,0 +1,18 @@
+# Creating a list in Python
+fruits = ["apple", "banana", "cherry"]
+
+# Adding an item to the list
+fruits.append("orange")
+
+# Removing an item from the list
+fruits.remove("banana")
+
+# Accessing items in the list
+first_fruit = fruits[0]
+
+# Looping through the list
+for fruit in fruits:
+    print(fruit)
+
+# Output the final list
+print("Final list:", fruits)
