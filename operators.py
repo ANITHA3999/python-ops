e05950f1524c014d27f9ef4549da7ce91132db74
ANITHA3999 <anithareddy90900@gmail.com>
2025-07-12 12:41:35 +0530

    upadated content

---
+++ operators.py
@@ -0,0 +1,14 @@
+# operators.py
+
+# Arithmetic Operators
+a = 10
+b = 3
+
+print("Arithmetic Operators:")
+print("a + b =", a + b)   # Addition
+print("a - b =", a - b)   # Subtraction
+print("a * b =", a * b)   # Multiplication
+print("a / b =", a / b)   # Division
+print("a % b =", a % b)   # Modulus
+print("a ** b =", a ** b) # Exponentiation
+print("a // b =", a // b) # Floor Division
